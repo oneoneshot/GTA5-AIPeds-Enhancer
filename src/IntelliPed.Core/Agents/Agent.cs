b3739f078ef1e09b2fe01770cdef4320889d76a8
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-06-16 16:06:14 +0000

    :pencil: update readme [skip ci]

--- src/IntelliPed.Core/Agents/Agent.cs
+++ src/IntelliPed.Core/Agents/Agent.cs
@@ -27,4 +27,6 @@ public class Agent
         kernelBuilder.Services.AddSingleton(this);
         kernelBuilder.Services.AddLogging(_ => _
             .SetMinimumLevel(LogLevel.Trace)
-            .AddD
+            .AddDebug()
+            .AddConsole());
+        kernelBuilder.AddOpenA
