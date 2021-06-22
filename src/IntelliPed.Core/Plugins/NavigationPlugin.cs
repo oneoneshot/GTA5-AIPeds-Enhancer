635335083f960f4971010e4677598e1dc7716c11
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-06-22 13:06:53 +0000

    getting closer...

--- src/IntelliPed.Core/Plugins/NavigationPlugin.cs
+++ src/IntelliPed.Core/Plugins/NavigationPlugin.cs
@@ -19,3 +19,38 @@ public class NavigationPlugin
 
         await agent.HubConnection.InvokeAsync("MoveToPosition", new MoveToPositionRequest
         {
+            X = coordinates.X,
+            Y = coordinates.Y,
+            Z = coordinates.Z,
+        });
+
+        Console.WriteLine($"Successfully navigated to ({coordinates.X}, {coordinates.Y}, {coordinates.Z})");
+        return "Successfully navigated.";
+    }
+
+    [KernelFunction]
+    [Description("Returns the co-ordinates of the specified location.")]
+    [return: Description("The co-ordinates of the location in the format of: (x, y, z).")]
+    public async Task<Coordinates> GetLocation(
+        Kernel kernel,
+        [Description("The location.")] string location)
+    {
+        await Task.Delay(250);
+        Console.WriteLine($"The co-ordinates of {location} are (-831, 172, 70)");
+        return new Coordinates(-831, 172, 70);
+    }
+}
+
+public class Coordinates
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public Coordinates(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
