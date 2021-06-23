9cc343120e6b68e51927c0a619f5b285233395e6
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-06-23 08:07:43 +0000

    Update Client_PresentConfigureUI.lua

--- src/IntelliPed.Core/Plugins/SpeechPlugin.cs
+++ src/IntelliPed.Core/Plugins/SpeechPlugin.cs
@@ -1,4 +1,7 @@
 ﻿using System.ComponentModel;
 using IntelliPed.Core.Agents;
 using IntelliPed.Messages.Speech;
-using Microsoft.AspNetCore.SignalR.Clien
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.SemanticKernel;
+
+namespace Intelli
