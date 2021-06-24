cc6ed8beb225ebd40797a3f56a78af91d6ae72d3
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-06-24 05:53:28 +0000

    chore: code cleanup

--- src/IntelliPed.Core/Plugins/SpeechPlugin.cs
+++ src/IntelliPed.Core/Plugins/SpeechPlugin.cs
@@ -4,4 +4,9 @@ using IntelliPed.Messages.Speech;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.SemanticKernel;
 
-namespace Intelli
+namespace IntelliPed.Core.Plugins;
+
+public class SpeechPlugin
+{
+    [KernelFunction]
+    
