3a269d45d5f028f3559f374a6f92cd403626ee03
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-07-20 15:40:14 +0000

    Unreviewed, update chromium test expectations.

--- src/IntelliPed.Core/Signals/SignalProcessor.cs
+++ src/IntelliPed.Core/Signals/SignalProcessor.cs
@@ -58,4 +58,4 @@ public class SignalProcessor
                  Your decisions must always be made independently without seeking user assistance. 
                  Play to your strengths as an LLM and pursue simple strategies with no legal complications.
                  
-                
+                 You must make use of your reasoning and decision-making capabi
