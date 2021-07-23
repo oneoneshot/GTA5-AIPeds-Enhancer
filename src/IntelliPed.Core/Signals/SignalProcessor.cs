53a005ac9a7baa1d1ba66b7102507493ee3a8ae3
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-07-23 20:09:04 +0000

    SCSI: libiscsi: fix potential buffer overrun in __iscsi_conn_send_pdu

--- src/IntelliPed.Core/Signals/SignalProcessor.cs
+++ src/IntelliPed.Core/Signals/SignalProcessor.cs
@@ -59,4 +59,6 @@ public class SignalProcessor
                  Play to your strengths as an LLM and pursue simple strategies with no legal complications.
                  
                  You must make use of your reasoning and decision-making capabilities to respond to the signal.
-                 Be realistic and think about what a ped would do in this si
+                 Be realistic and think about what a ped would do in this situation.
+                 
+                 
