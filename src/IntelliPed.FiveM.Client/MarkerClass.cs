cb8ebf03fa235279b62cdb5a2706f674d53706dc
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-01 16:28:03 +0000

    Create 7.html

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -2,4 +2,5 @@
 
 /*
  * The purpose of MarkerClass.cs is to provide compatibility support for language features
- * and attributes that are not available in earlier
+ * and attributes that are not available in earlier versions of .NET. This file defines
+ * several types and attributes tha
