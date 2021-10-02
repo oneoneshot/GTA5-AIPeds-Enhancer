6e1f9e38cd441275d04ef31a2f33d2c59d83fe2b
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-02 18:00:46 +0000

    enable debug

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -4,4 +4,6 @@
  * The purpose of MarkerClass.cs is to provide compatibility support for language features
  * and attributes that are not available in earlier versions of .NET. This file defines
  * several types and attributes that enable newer C# language features to be used in projects
- * targeting older versions of the .NET fram
+ * targeting older versions of the .NET framework.
+ *
+ * These conditional definitions ensure that
