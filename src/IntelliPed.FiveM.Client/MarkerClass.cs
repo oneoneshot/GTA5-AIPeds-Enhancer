80876390be428836bec300e658e1ab00c612f44c
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-02 14:13:06 +0000

    update FSF address

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -3,4 +3,5 @@
 /*
  * The purpose of MarkerClass.cs is to provide compatibility support for language features
  * and attributes that are not available in earlier versions of .NET. This file defines
- * several types and attributes that enable newer C# language features to be used in proje
+ * several types and attributes that enable newer C# language features to be used in projects
+ * targeting older versions of the .NET fram
