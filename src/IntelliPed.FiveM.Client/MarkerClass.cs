d73c4aeac1780ec6409596df0a4281d43e1472af
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-14 15:05:14 +0000

    Update things

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -21,4 +21,5 @@ namespace System.Runtime.CompilerServices
 
 #if !NET7_0_OR_GREATER
 
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherite
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class RequiredMemberAttribute : 
