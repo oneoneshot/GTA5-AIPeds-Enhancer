6e55584de7e3d6e07d63bcf9eee56eb88a9ee115
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-16 19:25:32 +0000

    revert accidental change

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -22,4 +22,6 @@ namespace System.Runtime.CompilerServices
 #if !NET7_0_OR_GREATER
 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-    internal sealed class RequiredMemberAttribute : 
+    internal sealed class RequiredMemberAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.All,
