54d8dc3e3dbb6980214d30c4e193ba892d4ada44
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-16 21:18:22 +0000

    add forgotten file

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -24,4 +24,5 @@ namespace System.Runtime.CompilerServices
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
     internal sealed class RequiredMemberAttribute : Attribute { }
 
-    [AttributeUsage(AttributeTargets.All,
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    internal sealed class C
