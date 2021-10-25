391f8a46262d4adb867610a841a38bb8a8a88402
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-25 14:59:24 +0000

    Clean up code style

--- src/IntelliPed.FiveM.Client/MarkerClass.cs
+++ src/IntelliPed.FiveM.Client/MarkerClass.cs
@@ -46,4 +46,5 @@ namespace System.Diagnostics.CodeAnalysis
 {
 #if !NET7_0_OR_GREATER
     [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
-    internal sealed c
+    internal sealed class SetsRequiredMembersAttribute : Attribute { }
+#endif
