87f4c6b7f4d3a8fb4ed59702b2882266395d53af
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-10-31 17:18:23 +0000

    bower fix

--- src/IntelliPed.FiveM.Client/Models/Color.cs
+++ src/IntelliPed.FiveM.Client/Models/Color.cs
@@ -6,4 +6,5 @@ public record Color(int R, int G, int B, int A = 255)
     public static Color Blue => new(0, 255, 0);
     public static Color Green => new(0, 0, 255);
     public static Color White => new(255, 255, 255);
-    public stat
+    public static Color Black => new(0, 0, 0);
+    public static Color
