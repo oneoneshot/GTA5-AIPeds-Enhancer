517b6c4335fd3ad9749372e2005ec72a80e6a001
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2021-11-05 19:18:13 +0000

    Fix variables

--- src/IntelliPed.FiveM.Client/Models/PedTextDraw.cs
+++ src/IntelliPed.FiveM.Client/Models/PedTextDraw.cs
@@ -6,4 +6,6 @@ public class PedTextDraw : TextDraw
 {
     private readonly Ped _ped;
 
-    public PedTextDraw(Ped ped, string text, Color? color = null) : base(ped.Posit
+    public PedTextDraw(Ped ped, string text, Color? color = null) : base(ped.Position, text, color)
+    {
+        _ped = ped
