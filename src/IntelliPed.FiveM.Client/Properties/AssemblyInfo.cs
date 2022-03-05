734533b8cc3a084b66de69d844ac0469b87f3f68
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2022-03-05 19:47:12 +0000

    Simple

--- src/IntelliPed.FiveM.Client/Properties/AssemblyInfo.cs
+++ src/IntelliPed.FiveM.Client/Properties/AssemblyInfo.cs
@@ -16,4 +16,7 @@ using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on t
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// T
