d64dd964325ba7efd34f9c502a0e4ea5a4be979e
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2022-03-06 16:43:46 +0000

    IEEE Sync

--- src/IntelliPed.FiveM.Client/Properties/AssemblyInfo.cs
+++ src/IntelliPed.FiveM.Client/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// T
+// The following GUID is for the ID of the typelib if thi
