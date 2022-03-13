6331b222f791e96187b69044fc01353992169925
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2022-03-13 22:18:04 +0000

    emails

--- src/IntelliPed.FiveM.Client/Properties/AssemblyInfo.cs
+++ src/IntelliPed.FiveM.Client/Properties/AssemblyInfo.cs
@@ -29,4 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revi
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: Assembl
