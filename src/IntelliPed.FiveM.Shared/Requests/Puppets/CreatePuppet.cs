6239ff7076932c010ae17de82900b3f4e89f5c51
oneoneshot <129029070+oneoneshot@users.noreply.github.com>
2022-07-01 05:28:33 +0000

    Tiny typo

--- src/IntelliPed.FiveM.Shared/Requests/Puppets/CreatePuppet.cs
+++ src/IntelliPed.FiveM.Shared/Requests/Puppets/CreatePuppet.cs
@@ -7,3 +7,10 @@ public record CreatePuppetRpcRequest : IClientRequest<CreatePuppetRpcReply>
 {
     public required float X { get; init; }
     public required float Y { get; init; }
+    public required float Z { get; init; }
+}
+
+public record CreatePuppetRpcReply
+{
+    public required int PedNetworkId { get; init; }
+}
